d2059cfec7e2edacbf6a1c0227f8ffb12a716384
Dhouglas <dhouglas.ricardo@gmail.com>
2020-11-04 16:15:01 -0300

    passa pooling notificações para o notifications.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,24 +14,3 @@
 //= require activestorage
 //= require materialize-sprockets
 //= require_tree .
-
-function getCountNewNotifications () {
-    $.ajax({
-        url: '/notifications/get_new_count',
-        success: function (response) {
-            var span = $('#menu_notification_item').find('span.secondary-content');
-            span.html('');
-            span.removeClass('new badge');
-
-            if (response.status == 'success') {
-
-                if (response.count > 0) {
-                    span.html(response.count);
-                    span.addClass('new badge');
-                }
-            }
-        }
-    })
-}
-
-setInterval(getCountNewNotifications, 15000);
--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -8,4 +8,25 @@ $(function () {
             data: { notification_id: notification_id }
         })
     });
+
+    function getCountNewNotifications () {
+        $.ajax({
+            url: '/notifications/get_new_count',
+            success: function (response) {
+                var span = $('#menu_notification_item').find('span.secondary-content');
+                span.html('');
+                span.removeClass('new badge');
+
+                if (response.status == 'success') {
+
+                    if (response.count > 0) {
+                        span.html(response.count);
+                        span.addClass('new badge');
+                    }
+                }
+            }
+        })
+    }
+
+    setInterval(getCountNewNotifications, 15000);
 })
